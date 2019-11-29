1068754fd3f5639c76353222148578e3624b869d
Bernadette Davis <bsd231@nyu.edu>
2019-11-29 18:00:22 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191129173048) do
+ActiveRecord::Schema.define(version: 20191129173012) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 20191129173048) do
     t.datetime "updated_at",       null: false
     t.integer  "category_id"
     t.string   "published_status"
-    t.string   "post_status"
+    t.boolean  "post_status"
   end
 
 end
